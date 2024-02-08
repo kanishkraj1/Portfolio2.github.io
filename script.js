24514f76c079da0e7f97036f83df064678272b2e
Kanishk Raj <kanishkraj1@gmail.com>
2024-02-08 17:19:15 +0530

    final toggle js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function buttonClick(){
-    var menu = document.getElementById('navID');
-    if(menu.className === 'navBar menuoff'){
-        menu.className = 'navBar menuon';
-    } else{
-        menu.className = 'navBar menuoff';
+    const menu = document.getElementsByClassName('navBar');
+    const bttn =  document.getElementById('hamButton');
+    var x = document.getElementById("navID");
+    if (x.style.display === "none") {
+      x.style.display = "block";     
+      x.style.position = "relative";
+      x.style.textAlign = "center";
+      x.style.width = "100%";
+      x.style.display = "flex";
+      x.flexDirection = "column";
+
+    } else {
+      x.style.display = "none";
     }
 }
